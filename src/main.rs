03ed3f69b5ee7ee5856a4032fe50849b8b8ce2e7
Zhi Wang <zwang@cs.fsu.edu>
2018-02-20 22:42:11 -0500

    save

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-#[macro_use] extern crate conrod;
+#[macro_use]
+extern crate conrod;
 mod gui;
 
 fn main() {
     feature::main();
 }
-
 
 mod feature {
     use conrod;
@@ -39,13 +39,16 @@ mod feature {
         let ids = Ids::new(ui.widget_id_generator());
 
         // Add a `Font` to the `Ui`'s `font::Map` from file.
-       let noto_sans: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/assets/fonts/NotoSans/");
-
+        let noto_sans: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/assets/fonts/NotoSans/");
 
         // Store our `font::Id`s in a struct for easy access in the `set_ui` function.
         let fonts = Fonts {
-            regular: ui.fonts.insert_from_file(format!("{}{}", noto_sans, "NotoSans-Regular.ttf")).unwrap(),
-            mono: ui.fonts.insert_from_file(format!("{}{}", noto_sans, "NotoSans-Italic.ttf")).unwrap(),
+            regular: ui.fonts
+                .insert_from_file(format!("{}{}", noto_sans, "NotoSans-Regular.ttf"))
+                .unwrap(),
+            mono: ui.fonts
+                .insert_from_file(format!("{}{}", noto_sans, "NotoSans-Italic.ttf"))
+                .unwrap(),
         };
 
         // Specify the default font to use when none is specified by the widget.
@@ -61,28 +64,27 @@ mod feature {
         // The image map describing each of our widget->image mappings (in our case, none).
         let image_map = conrod::image::Map::<glium::texture::Texture2d>::new();
 
-        
         // Poll events from the window.
         let mut event_loop = gui::EventLoop::new();
         'main: loop {
-
             // Handle all events.
             for event in event_loop.next(&mut events_loop) {
-
                 // Use the `winit` backend feature to convert the winit event to a conrod one.
-                if let Some(event) = conrod::backend::winit::convert_event(event.clone(), &display) {
+                if let Some(event) = conrod::backend::winit::convert_event(event.clone(), &display)
+                {
                     ui.handle_event(event);
                 }
 
                 match event {
                     glium::glutin::Event::WindowEvent { event, .. } => match event {
                         // Break from the loop upon `Escape`.
-                        glium::glutin::WindowEvent::Closed |
-                        glium::glutin::WindowEvent::KeyboardInput {
-                            input: glium::glutin::KeyboardInput {
-                                virtual_keycode: Some(glium::glutin::VirtualKeyCode::Escape),
-                                ..
-                            },
+                        glium::glutin::WindowEvent::Closed
+                        | glium::glutin::WindowEvent::KeyboardInput {
+                            input:
+                                glium::glutin::KeyboardInput {
+                                    virtual_keycode: Some(glium::glutin::VirtualKeyCode::Escape),
+                                    ..
+                                },
                             ..
                         } => break 'main,
                         _ => (),
@@ -108,17 +110,27 @@ mod feature {
     widget_ids!{
         struct Ids {
             master,
-            header, 
-            body, 
+            header,
+            body,
             footer,
             col_code,
+            col_code_title,
+            col_code_main,
             col_mem,
+            col_mem_title,
+            col_mem_main,
             col_stack,
+            col_stack_title,
+            col_stack_main,
             col_reg,
+            col_reg_title,
+            col_reg_top,
+            col_reg_bottom,
             label_code,
             label_stack,
             label_mem,
             label_reg,
+            list_code,
             left_text,
             middle_text,
             right_text,
@@ -133,21 +145,80 @@ mod feature {
         use conrod::{color, widget, Colorable, Positionable, Sizeable, Widget};
 
         // Our `Canvas` tree, upon which we will place our text widgets.
-        widget::Canvas::new().flow_down(&[
-            (ids.header, widget::Canvas::new().length(50.0).color(color::LIGHT_BLUE)),
-            (ids.body, widget::Canvas::new().flow_right(&[
-                (ids.col_code, widget::Canvas::new().length_weight(52.0).color(color::LIGHT_CHARCOAL)),
-                (ids.col_mem, widget::Canvas::new().length_weight(16.0).color(color::WHITE)),
-                (ids.col_stack, widget::Canvas::new().length_weight(16.0).color(color::WHITE)),
-                (ids.col_reg, widget::Canvas::new().length_weight(16.0).color(color::WHITE)),
-            ])),
-        ]).set(ids.master, ui);
-
+        widget::Canvas::new()
+            .flow_down(&[
+                (
+                    ids.header, //header to hold buttons
+                    widget::Canvas::new().length(50.0).color(color::LIGHT_BLUE),
+                ),
+                (
+                    ids.body,
+                    widget::Canvas::new().flow_right(&[
+                        (
+                            ids.col_code, // the canvas to show the code
+                            widget::Canvas::new().length_weight(52.0).flow_down(&[
+                                (
+                                    ids.col_code_title,
+                                    widget::Canvas::new().length(36.0).color(color::BLUE),
+                                ),
+                                (ids.col_code_main, widget::Canvas::new().color(color::WHITE)),
+                            ]),
+                        ),
+                        (
+                            ids.col_mem,
+                            widget::Canvas::new().length_weight(16.0).flow_down(&[
+                                (
+                                    ids.col_mem_title,
+                                    widget::Canvas::new().length(36.0).color(color::BLUE),
+                                ),
+                                (ids.col_mem_main, widget::Canvas::new().color(color::WHITE)),
+                            ]),
+                        ),
+                        (
+                            ids.col_stack,
+                            widget::Canvas::new().length_weight(16.0).flow_down(&[
+                                (
+                                    ids.col_stack_title,
+                                    widget::Canvas::new().length(36.0).color(color::BLUE),
+                                ),
+                                (
+                                    ids.col_stack_main,
+                                    widget::Canvas::new().color(color::WHITE),
+                                ),
+                            ]),
+                        ),
+                        (
+                            ids.col_reg,
+                            widget::Canvas::new().length_weight(16.0).flow_down(&[
+                                (
+                                    ids.col_reg_title,
+                                    widget::Canvas::new().length(36.0).color(color::BLUE),
+                                ),
+                                (
+                                    ids.col_reg_top,
+                                    widget::Canvas::new()
+                                        .length_weight(50.0)
+                                        .color(color::WHITE),
+                                ),
+                                (
+                                    ids.col_reg_bottom,
+                                    widget::Canvas::new()
+                                        .length_weight(50.0)
+                                        .color(color::LIGHT_BLUE),
+                                ),
+                            ]),
+                        ),
+                    ]),
+                ),
+            ])
+            .set(ids.master, ui);
 
         // Creat buttons to control the execution
-        let btn = widget::Button::new().color(color::LIGHT_GREY).w_h(50.0, 36.0);
+        let btn = widget::Button::new()
+            .color(color::LIGHT_GREY)
+            .w_h(50.0, 36.0);
         let btn_next = btn.clone().label("Next").middle_of(ids.header);
-        let btn_run = btn.label("Run").right_from(ids.btn_next, 10.0);        
+        let btn_run = btn.label("Run").right_from(ids.btn_next, 10.0);
 
         for _click in btn_next.set(ids.btn_next, ui) {
             println!("Next instruction");
@@ -156,18 +227,20 @@ mod feature {
         for _click in btn_run.set(ids.btn_run, ui) {
             println!("Run until end");
         }
-        
-        fn title (tb: widget::TextBox, parent_id: widget::Id) -> widget::TextBox { 
-            tb.color(color::DARK_BLUE).center_justify().text_color(color::WHITE).
-                font_size(20).w_of(parent_id).mid_top_of(parent_id)
+
+        fn title(txt: widget::Text, parent_id: widget::Id) -> widget::Text {
+            txt.color(color::WHITE)
+                .center_justify()
+                .font_size(20)
+                .middle_of(parent_id)
         };
 
-        title(widget::TextBox::new("Code"), ids.col_code).set(ids.label_code, ui);
-        title(widget::TextBox::new("Stack"), ids.col_stack).set(ids.label_stack, ui);
-        title(widget::TextBox::new("Mem"), ids.col_mem).set(ids.label_mem, ui);
-        title(widget::TextBox::new("Registers"), ids.col_reg).set(ids.label_reg, ui);
+        title(widget::Text::new("Code"), ids.col_code_title).set(ids.label_code, ui);
+        title(widget::Text::new("Stack"), ids.col_stack_title).set(ids.label_stack, ui);
+        title(widget::Text::new("Mem"), ids.col_mem_title).set(ids.label_mem, ui);
+        title(widget::Text::new("Registers"), ids.col_reg_title).set(ids.label_reg, ui);
 
-        let _code_lines = [
+        let code_lines = [
             "if let Some(primitives) = ui.draw_if_changed() {".to_string(),
             "    renderer.fill(&display, primitives, &image_map);".to_string(),
             "    let mut target = display.draw();".to_string(),
@@ -176,6 +249,21 @@ mod feature {
             "    target.finish().unwrap();".to_string(),
             "}".to_string(),
         ];
+
+        let (mut items, scrollbar) = widget::List::flow_down(code_lines.len())
+            .scrollbar_on_top()
+            .middle_of(ids.col_code_main)
+            .padded_wh_of(ids.col_code_main, 10.0)
+            .set(ids.list_code, ui);
+
+        while let Some(item) = items.next(ui) {
+            let i = item.i;
+            item.set(widget::Text::new(&code_lines[i]).font_size(24), ui);
+        }
+
+        if let Some(sbar) = scrollbar {
+            sbar.set(ui);
+        }
     }
 
 }
